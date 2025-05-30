9346a937884250607cbeb23635f89c71d8885ade
Trey G <trey.gaines@outlook.com>
2025-05-30 09:51:39 -0400

    Initial Commit

---
+++ MyAlarm/MyAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAlarmApp.swift
+//  MyAlarm
+//
+//  Created by Trey Gaines on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct MyAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyAlarmTests/MyAlarmTests.swift
@@ -0,0 +1,16 @@
+//
+//  MyAlarmTests.swift
+//  MyAlarmTests
+//
+//  Created by Trey Gaines on 5/30/25.
+//
+
+import Testing
+
+struct MyAlarmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
